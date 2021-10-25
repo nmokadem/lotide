9290eac0a5d8d89b5ab1fb72eb47e82eb223ae2d
Nasser Mokadem <nmokadem@hotmail.com>
2021-10-25 21:17:57 +0000

    add a function that compare the head of an array with another value

--- head.js
+++ head.js
@@ -7,6 +7,12 @@ const assertEqual = function(actual, expected) {
   }
 };
 
-// TEST CODE
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
+
+const head = function(arr) {
+  return arr[0];
+}
+
+
+
+assertEqual(head([5,6,7]), 5);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
