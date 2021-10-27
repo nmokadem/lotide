e3551ba116cf0a93789c4194ac0e972a2c7b62b0
Nasser Mokadem <nmokadem@hotmail.com>
2021-10-27 22:27:35 +0000

    Add new script assertObjectEqual.js

---
+++ assertObjectsEqual.js
@@ -0,0 +1,66 @@
+const printMessage = function(actual, expected, checkEqual) {
+  if (checkEqual) {
+    console.log("✅✅✅ Assertion Passed: ", actual, " === ", expected);
+  } else {
+    console.log("🛑🛑🛑 Assertion Failed: ", actual, " !== ", expected);
+  }
+};
+
+const assertEqual = function(actual, expected) {
+  return actual === expected;
+};
+
+const eqArrays = function(actual, expected) {
+  let flag = true;
+
+  if (Array.isArray(actual) && Array.isArray(expected) && actual.length === expected.length) {
+    for (let i = 0; i < actual.length; i++) {
+      if (assertEqual(actual[i], expected[i])) continue;
+      flag = false;
+      break;
+    }
+  } else {
+    flag = false;
+  }
+
+  return flag;
+};
+
+// Returns true if both objects have identical keys with identical values.
+// Otherwise you get back a big fat false!
+const eqObjects = function(object1, object2) {
+  let keys1 = Object.keys(object1);
+  let keys2 = Object.keys(object2);
+
+  if (!assertEqual(keys1.length, keys2.length)) return false;
+
+  for (let key of keys1) {
+    if (Array.isArray(object1[key])){
+      if (!eqArrays(object1[key],object2[key])) return false;
+    } else {
+        if (!assertEqual(object1[key],object2[key])) return false;
+    }
+  }
+  return true;
+};
+
+const assertObjectsEqual = function(actual, expected) {
+  // const inspect = require('util').inspect; // <= add this line
+  // console.log(`Example label: ${inspect(actual)}`);
+  printMessage(actual, expected, eqObjects(actual, expected));
+};
+
+
+const ab = { a: "1", b: "2" };
+const ba = { b: "2", a: "1" };
+assertObjectsEqual(ab, ba); // => true
+
+const abc = { a: "1", b: "2", c: "3" };
+assertObjectsEqual(ab, abc); // => false
+
+const cd = { c: "1", d: ["2", 3] };
+const dc = { d: ["2", 3], c: "1" };
+assertObjectsEqual(cd, dc); // => true
+
+const cd2 = { c: "1", d: ["2", 3, 4] };
+assertObjectsEqual(cd, cd2); // => false
