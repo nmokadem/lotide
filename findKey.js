db39d119b3b912a5bdd714b8b8b0f3e097d451a8
Nasser Mokadem <nmokadem@hotmail.com>
2021-10-28 22:09:15 +0000

    Add script findKey.js

---
+++ findKey.js
@@ -0,0 +1,41 @@
+const printMessage = function(actual, expected, checkEqual) {
+  if (checkEqual) {
+    console.log("✅✅✅ Assertion Passed: ", actual, " === ", expected);
+  } else {
+    console.log("🛑🛑🛑 Assertion Failed: ", actual, " !== ", expected);
+  }
+};
+
+const assertEqual = function(actual, expected) {
+  let checkEqual = actual === expected;
+  printMessage(actual, expected, checkEqual);
+};
+
+
+const findKey = function (obj, callback) {
+  const keys = Object.keys(obj);
+  for (let key of keys){
+    if (callback(obj[key])) return key;
+  }
+  return false;
+}
+
+// findKey({
+//   "Blue Hill": { stars: 1 },
+//   "Akaleri":   { stars: 3 },
+//   "noma":      { stars: 2 },
+//   "elBulli":   { stars: 3 },
+//   "Ora":       { stars: 2 },
+//   "Akelarre":  { stars: 3 }
+// }, x => x.stars === 2); // => "noma"
+
+assertEqual(findKey(
+  {
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+  },
+    x => x.stars === 2), "noma");
