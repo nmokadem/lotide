b7c889860bbc57e92706f45d736130b632d0dcda
Nasser Mokadem <nmokadem@hotmail.com>
2021-10-28 21:16:05 +0000

    Test for the quality of arrays: the returned array and waht is expected

--- takeUntil.js
+++ takeUntil.js
@@ -40,7 +40,6 @@ const takeUntil = function(array, callback) {
   return results;
 }
 
-
 const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
 const results1 = takeUntil(data1, x => x < 0);
 console.log(results1);
@@ -51,6 +50,8 @@ const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Re
 const results2 = takeUntil(data2, x => x === ',');
 console.log(results2);
 
+
+assertArraysEqual(takeUntil([1, 2, 5, 7, 2, -1, 2, 4, 5], x => x < 0),[1, 2, 5, 7, 2]);
 /*
 [ 1, 2, 5, 7, 2 ]
 ---
