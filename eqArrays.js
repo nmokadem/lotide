02487d24fbae591ebeac70e2c976c923c61fb4f3
Nasser Mokadem <nmokadem@hotmail.com>
2021-10-29 21:00:53 +0000

    Fix eslint errors

--- eqArrays.js
+++ eqArrays.js
@@ -1,12 +1,13 @@
 const assertEqual = function(actual, expected) {
   return actual === expected;
-}
+};
 
 const eqArrays = function(actual, expected) {
   let flag = true;
 
   if (Array.isArray(actual) && Array.isArray(expected) && actual.length === expected.length) {
     for (let i = 0; i < actual.length; i++) {
+      
       if (Array.isArray(actual[i])) {
         if (!eqArrays(actual[i],expected[i])) return false;
         continue;
@@ -32,6 +33,4 @@ console.log(eqArrays(["1", "2", "3"], ["1", "2", 3])); // => false
 
 console.log(assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true)); // => should PASS
 
-console.log(eqArrays([[2, 3], [4]], [[2, 3], [4]]),
-            eqArrays([[2, 3], [4]], [[2, 3], [4, 5]]),
-            eqArrays([[2, 3], [4]], [[2, 3], 4]));
+console.log(eqArrays([[2, 3], [4]], [[2, 3], [4]]), eqArrays([[2, 3], [4]], [[2, 3], [4, 5]]), eqArrays([[2, 3], [4]], [[2, 3], 4]));
