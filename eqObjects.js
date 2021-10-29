795ec1707ab9524a1fe85a4f951ffa1f91c9e33c
Nasser Mokadem <nmokadem@hotmail.com>
2021-10-29 20:22:05 +0000

    modifiy eqObjects.js to handle nested objects

--- eqObjects.js
+++ eqObjects.js
@@ -31,10 +31,16 @@ const eqArrays = function(actual, expected) {
 const eqObjects = function(object1, object2) {
   let keys1 = Object.keys(object1);
   let keys2 = Object.keys(object2);
+  let obj = {};
 
-  if (!assertEqual(keys1.length, keys2.length)) return false;
+  for (let key of keys1) { 
+    obj = object1[key];
+    if (typeof obj === "object" && !Array.isArray(obj) && obj !== null) {
+      console.log(key,obj,object2[key]);
+      if (!eqObjects(obj,object2[key])) return false;
+      continue;
+    }
 
-  for (let key of keys1) {
     if (Array.isArray(object1[key])){
       if (!eqArrays(object1[key],object2[key])) return false;
     } else {
@@ -45,16 +51,21 @@ const eqObjects = function(object1, object2) {
 };
 
 
-const ab = { a: "1", b: "2" };
-const ba = { b: "2", a: "1" };
-console.log(eqObjects(ab, ba)); // => true
+// const ab = { a: "1", b: "2" };
+// const ba = { b: "2", a: "1" };
+// console.log(eqObjects(ab, ba)); // => true
 
-const abc = { a: "1", b: "2", c: "3" };
-console.log(eqObjects(ab, abc)); // => false
+// const abc = { a: "1", b: "2", c: "3" };
+// console.log(eqObjects(ab, abc)); // => false
 
-const cd = { c: "1", d: ["2", 3] };
-const dc = { d: ["2", 3], c: "1" };
-console.log(eqObjects(cd, dc)); // => true
+// const cd = { c: "1", d: ["2", 3] };
+// const dc = { d: ["2", 3], c: "1" };
+// console.log(eqObjects(cd, dc)); // => true
 
-const cd2 = { c: "1", d: ["2", 3, 4] };
-console.log(eqObjects(cd, cd2)); // => false
+// const cd2 = { c: "1", d: ["2", 3, 4] };
+// console.log(eqObjects(cd, cd2)); // => false
+
+console.log(eqObjects({ a: { z: 1 }, b: 2 }, { a: { z: 1 }, b: 2 })); // => true
+
+console.log(eqObjects({ a: { y: 0, z: 1 }, b: 2 }, { a: { z: 1 }, b: 2 })); // => false
+console.log(eqObjects({ a: { y: 0, z: 1 }, b: 2 }, { a: 1, b: 2 })); // => false
