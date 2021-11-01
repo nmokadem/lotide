269b0797414d486a35d886258f419ce2734f5226
Nasser Mokadem <nmokadem@hotmail.com>
2021-11-01 21:03:23 +0000

    Test with mocha and chai

--- test/headTest.js
+++ test/headTest.js
@@ -1,8 +1,25 @@
 // test/headTest.js
 
-const head = require('../head');
-const {assertEqual, printMessage} = require('../assertEqual');
+const assert = require('chai').assert;
+const head   = require('../head');
 
-// TEST CODE
-printMessage([5,6,7], 5,assertEqual(head([5,6,7]), 5));
-printMessage(["Hello", "Lighthouse", "Labs"], "Hello",assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello"));
+describe("#head", () => {
+  it("returns 1 for [1, 2, 3]", () => {
+    assert.strictEqual(head([1, 2, 3]), 1);
+  });
+  it("returns '5' for ['5']", () => {
+    assert.strictEqual(head(['5']), '5'); 
+  });
+  it("returns 'Hello' for ['Hello','Lighthouse','Labs']", () => {
+    assert.strictEqual(head(['Hello','Lighthouse','Labs']), 'Hello'); 
+  });  
+});
+
+
+
+// const head = require('../head');
+// const {assertEqual, printMessage} = require('../assertEqual');
+
+// // TEST CODE
+// printMessage([5,6,7], 5,assertEqual(head([5,6,7]), 5));
+// printMessage(["Hello", "Lighthouse", "Labs"], "Hello",assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello"));
